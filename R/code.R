8904a58901fc06b8c75990e426741699ba313d16
lmaowsic <lmao@biostat.wisc.edu>
2023-09-18 10:27:47 -0500

    Initial commit

--- R/code.R
+++ R/code.R
@@ -6,3 +6,12 @@ use_git_config(
   user.email = "lmao@biostat.wisc.edu"
 )
 use_git()
+
+
+###
+
+use_r("lib-summary")
+
+# pkgs <- utils::installed.packages()
+# pkg_tbl <- table(pkgs[,"LibPath"])
+
